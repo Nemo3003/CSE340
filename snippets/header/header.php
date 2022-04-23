afb440d32385ea8f7c23a057f6c1b631971111b6
Ulises Mariano Melgarejo <73831325+Nemo3003@users.noreply.github.com>
2022-04-23 08:30:57 -0300

    Update header.php

--- snippets/header/header.php
+++ snippets/header/header.php
@@ -1,2 +1,5 @@
 <img src="/phpmotors/images/site/logo.png" alt="my logo">
 <h2 style="display: flex; justify-content:flex-end;margin-top: -2.3rem; ">PHP Motors</h2>
+<?php
+//here we have the header
+?>
