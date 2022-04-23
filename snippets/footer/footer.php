a6da87d7d27bd3b91a5ac7c933bfacde2cc83871
Ulises Mariano Melgarejo <73831325+Nemo3003@users.noreply.github.com>
2022-04-23 08:31:12 -0300

    Update footer.php

--- snippets/footer/footer.php
+++ snippets/footer/footer.php
@@ -3,3 +3,6 @@
             All images are believed to be in "Falt Use". Please notify the author if any are not and they will be removed <br>
             Last Updated <span id="last"></span>
         </p>
+<?php
+//here we have the footer
+?>
