cdeb23985b9e261209b4845cd2da653c6e0da012
Nemo3003 <ulimarian2000@gmail.com>
2022-04-24 13:01:00 -0300

    'homepage'

--- home.php
+++ home.php
@@ -9,17 +9,61 @@
 
 <body>
         <header id ="header_page">
-        <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/header/header.php'; ?> 
+        <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/header.php'; ?> 
         </header>
         <nav id ="page_nav" > 
-        <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/nav/nav.php'; ?> 
+        <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/nav.php'; ?> 
         </nav>
         <main>
         <h1>Welcome to PHP Motors!</h1> 
-        <hr>
+        <div id="delorian">
+            <div id="side-del">
+                <h2 id="title-del"> DMC Delorean</h2>
+                <p>3 Cup holders</p>
+                <p>Superman doors</p>
+                <p>Fuzzy dice</p>
+                <button>Own Today</button>
+                <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            </div>
+        </div>
+        <div id="div-bottom">
+            <div id="upgrades">
+            <h2>Delorean Upgrades</h2>
+             <div id="upg-content">
+                 <div id="upgr-deep">
+                    <div>
+                        <img src="/phpmotors/images/upgrades/bumper_sticker.jpg" alt="bumper_sticker"><br>
+                        <a  href="#">Bumper Sticker</a>
+                    </div>
+                    <div>
+                        <img src="/phpmotors/images/upgrades/flame.jpg" alt="flame"><br>
+                        <a href="#">flame</a>
+                    </div>
+                    <div>
+                        <img src="/phpmotors/images/upgrades/flux-cap.png" alt="flux-cap"><br>
+                        <a href="#">flux cap</a>
+                    </div>
+                    <div>
+                        <img src="/phpmotors/images/upgrades/hub-cap.jpg" alt="hub_cap"><br>
+                        <a href="#">hub cap</a>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div id="character">
+                <h2>DMC Delorean Reviews</h2>
+                <ul>
+                    <li>"So fast its almost like travelling in time." (4/5)</li><br>
+                    <li>"Coolest ride on the road."(4/5)</li><br>
+                    <li>"I'm feeling Marty McFly!"(5/5)</li><br>
+                    <li>"The most futuristic ride of our day."(4.5/5)</li><br>
+                    <li>"50's livin and I love it!"(5/5)</li><br>
+                </ul>
+            </div>
+        </div>
         </main>
     <footer id = "footer_page">
-    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/footer/footer.php'; ?> 
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/footer.php'; ?> 
     </footer>
     <script src = "js/app.js"></script>
 </body>
