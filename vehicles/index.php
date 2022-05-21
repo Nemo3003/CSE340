42632e913118de69c6269e6825dc67ad55964cc4
Ulises Mariano Melgarejo <73831325+Nemo3003@users.noreply.github.com>
2022-05-21 15:30:12 -0300

    enhancement 4: still working on it

--- vehicles/index.php
+++ vehicles/index.php
@@ -1,62 +1,84 @@
 <?php
-/**
- * http://lvh.me/phpmotors/.
- * instructions to run docker. put this into the terminal: docker compose up -d
- * VEHICLES CONTROLLER
- */
+/*THIS IS THE VEHICLES CONTROLLER*/
+    require_once '../library/connections.php';
+    require_once '../model/main-model.php';
+    require_once '../model/vehicles-model.php';
 
-// Get the database connection file
-require_once '../library/connections.php';
-// Get the PHP Motors model for use as needed
-require_once '../model/main-model.php';
-// Get the accounts model
-require_once '../model/vehicles-model.php';
-
-/**Create a $classificationList variable to build a dynamic drop-down select list. The classificationName must appear in the browser as an option to select, but the classificationId must be the value of each option. */
-$classificationList = '<select name="classificationId">';
-$classifications = getClassifications();
-foreach ($classifications as $classification) {
-    $classificationList .= "<option value='$classification[classificationId]'>$classification[classificationName]</option>";
-}
-$classificationList .= '</select>';
+    $classifications = getClassifications();
 
 
-// Get the array of classifications
-$classifications = getClassifications();
+    $navList = "<ul id='navigation'>";
+    $navList .= "<li><a href='/phpmotors/accounts/index.php' title='View the PHP Motors home page'>Home</a></li>";
+    $classificationList = "<select name='classificationId' id='carClassification'>";
+    foreach ($classifications as $classification) {$name = $classification['classificationName']; $id = $classification['classificationId']; $navList .="<li><a href='/phpmotors/index.php?action=".urlencode($name)."' title='View our $name product line'>$name</a></li>"; $classificationList .= "<option value='$id'>$name</option>";
+    }
+    $navList .='</ul>';
+    $classificationList .="</select><br>";
 
-// Build a navigation bar using the $classifications array
-$navList = '<ul>';
-$navList .= "<li><a href='/phpmotors/index.php' title='View the PHP Motors home page'>Home</a></li>";
-foreach ($classifications as $classification) {
- $navList .= "<li><a href='/phpmotors/index.php?action=".urlencode($classification['classificationName'])."' title='View our $classification[classificationName] product line'>$classification[classificationName]</a></li>";
-}
-$navList .= '</ul>';
-//echo $navList;
-//exit;
-/**echo "<pre>";
-var_dump($classifications);
-echo "</pre>";
-exit;*/
 
-// Get the value from the action name - value pair
-$action = filter_input(INPUT_POST, 'action');
-if ($action == NULL) {
-  $action = filter_input(INPUT_GET, 'action');
-}
 
-switch($action){
-    case 'something':
-        // do something
-        break;
-    case 'template':
-        include 'view/template.php';
-        break;
-    case 'vehicle':
+    $action = filter_input(INPUT_POST, 'action');
+    if ($action == NULL) {
+        $action = filter_input(INPUT_GET, 'action');
+    }
+    
+    switch ($action) {
+        case 'newClassification':
+            //filter_input gets an external variable (e.g. from form input) and optionally filters it.
+            $classificationName = filter_input(INPUT_POST, 'classificationName');
+            
+            if(empty($classificationName)) {
+                $message = '<p>Please provide information for all empty form fields.</p>';
+                include '../view/add-classification.php';
+                exit;
+            }
+            $Outcome = newClassification($classificationName);
+            if($Outcome === 1){
+                header("Location: ../vehicles/");
+                exit;
+            } else {
+                $message = "<p>Sorry, but the Submission failed. Please try again.</p>";
+                include '../view/add-classification.php';
+                exit;
+            }
+            break;
+        case 'newVehicle':
+            $invMake = filter_input(INPUT_POST, 'invMake');
+            $invModel = filter_input(INPUT_POST, 'invModel');
+            $invDescription = filter_input(INPUT_POST, 'invDescription');
+            $invImage = filter_input(INPUT_POST, 'invImage');
+            $invThumbnail = filter_input(INPUT_POST, 'invThumbnail');
+            $invPrice = filter_input(INPUT_POST, 'invPrice');
+            $invStock = filter_input(INPUT_POST, 'invStock');
+            $invColor = filter_input(INPUT_POST, 'invColor');
+            $classificationId = filter_input(INPUT_POST, 'classificationId');
+
+            if(empty($invMake) || empty($invModel) || empty($invDescription) || empty($invPrice) || empty($invStock) || empty($invColor) || empty($classificationId)) {
+                $message = '<p>Please provide information for all empty form fields.</p>';
+                include '../view/add-vehicle.php';
+                exit;
+            }
+
+            $Outcome = newVehicle($invMake, $invModel, $invDescription, $invImage, $invThumbnail, $invPrice, $invStock, $invColor, $classificationId);
+            if($Outcome === 1){
+                $message = "<p>Inventory update of $invMake $invModel successful. Current stock: $invStock Current price: $invPrice</p>";
+                include '../view/add-vehicle.php';
+                exit;
+            } else {
+                $message = "<p>Sorry, but the inventory update failed. Please try again.</p>";
+                include '../view/add-vehicle.php';
+                exit;
+            }
+            break;
+        case 'addClassification':
+            include '../view/add-classification.php';
+            break;
+        case 'addVehicle':
+            include '../view/add-vehicle.php';
+            break;
+        default:
             include '../view/vehicle-management.php';
             break;
-    case 'Addclassification':
-        include '../view/add-classification.php';
-        break;
-}
+    }
 
 ?>
