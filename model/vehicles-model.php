cb6e0c4c1925e0cdd7752e11f312d38404fb92d0
Ulises Mariano Melgarejo <73831325+Nemo3003@users.noreply.github.com>
2022-05-21 15:25:53 -0300

    Enhancement 4: not yet ready

--- model/vehicles-model.php
+++ model/vehicles-model.php
@@ -1,82 +1,42 @@
 <?php
-//printing a nice message about cars
-echo "<h1>Welcome to the Cars for Sale Page</h1>";
-//creating a small array to be displayed to the user
-$cars = array(
-    array(
-        "make" => "Ford",
-        "model" => "Mustang",
-        "year" => "1964",
-        "price" => "19,995",
-        "image" => "images/mustang.jpg"
-    ),
-    array(
-        "make" => "Chevrolet",
-        "model" => "Corvette",
-        "year" => "1964",
-        "price" => "19,995",
-        "image" => "images/corvette.jpg"
-    ),
-    array(
-        "make" => "Dodge",
-        "model" => "Charger",
-        "year" => "1964",
-        "price" => "19,995",
-        "image" => "images/charger.jpg"
-    ),
-    array(
-        "make" => "Ford",
-        "model" => "Mustang",
-        "year" => "1964",
-        "price" => "19,995",
-        "image" => "images/mustang.jpg"
-    ),
-    array(
-        "make" => "Chevrolet",
-        "model" => "Corvette",
-        "year" => "1964",
-        "price" => "19,995",
-        "image" => "images/corvette.jpg"
-    ),
-    array(
-        "make" => "Dodge",
-        "model" => "Charger",
-        "year" => "1964",
-        "price" => "19,995",
-        "image" => "images/charger.jpg"
-    ),
-    array(
-        "make" => "Ford",
-        "model" => "Mustang",
-        "year" => "1964",
-        "price" => "19,995",
-        "image" => "images/mustang.jpg"
-    ),
-    array(
-        "make" => "Chevrolet",
-        "model" => "Corvette",
-        "year" => "1964",
-        "price" => "19,995",
-        "image" => "images/corvette.jpg"
-    ),
-    array(
-        "make" => "Dodge",
-        "model" => "Charger",
-        "year" => "1964",
-        "price" => "19,995",
-        "image" => "images/charger.jpg"
-    ));
-//printing the array to the user
-echo "<table>";
-foreach ($cars as $car) {
-    echo "<tr>";
-    echo "<td><img src='" . $car['image'] . "'></td>";
-    echo "<td>";
-    echo "<h2>" . $car['make'] . " " . $car['model'] . "</h2>";
-    echo "<p>Year: " . $car['year'] . "</p>";
-    echo "<p>Price: " . $car['price'] . "</p>";
-    echo "</td>";
-    echo "</tr>";
+
+function newClassification($classificationName){
+    $db = phpmotorsConnect();
+    $sql = 'INSERT INTO carclassification (classificationName)
+        VALUES (:classificationName)';
+    //The PDO::PARAM_STR Represents the SQL CHAR, VARCHAR, or other string data type.
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(':classificationName', $classificationName, PDO::PARAM_STR);
+    $stmt->execute();
+    $rowsNew = $stmt->rowCount();
+    $stmt->closeCursor();
+    return $rowsNew;
 }
-echo "</table>";
+
+
+function newVehicle($invMake, $invModel, $invDescription, $invImage, $invThumbnail, $invPrice, $invStock, $invColor, $classificationId) {
+    $db = phpmotorsConnect();
+    $sql = 'INSERT INTO inventory (invMake, invModel, invDescription, invImage, invThumbnail, invPrice, invStock, invColor, classificationId)
+        VALUES (:invMake, :invModel, :invDescription, :invImage, :invThumbnail, :invPrice, :invStock, :invColor, :classificationId)';
+    
+    $stmt = $db->prepare($sql);
+    //bindValue() function is an inbuilt function in PHP which is used to bind a value to a parameter.
+    $stmt->bindValue(':invMake', $invMake, PDO::PARAM_STR);
+    $stmt->bindValue(':invModel', $invModel, PDO::PARAM_STR);
+    $stmt->bindValue(':invDescription', $invDescription, PDO::PARAM_STR);
+    $stmt->bindValue(':invImage', $invImage, PDO::PARAM_STR);
+    $stmt->bindValue(':invThumbnail', $invThumbnail, PDO::PARAM_STR);
+    $stmt->bindValue(':invPrice', $invPrice, PDO::PARAM_STR);
+    $stmt->bindValue(':invStock', $invStock, PDO::PARAM_STR);
+    $stmt->bindValue(':invColor', $invColor, PDO::PARAM_STR);
+    $stmt->bindValue(':classificationId', $classificationId, PDO::PARAM_STR);
+    
+    $stmt->execute();
+    
+    $rowsNew = $stmt->rowCount();
+    
+    $stmt->closeCursor();
+    
+    return $rowsNew;
+}
 ?>
