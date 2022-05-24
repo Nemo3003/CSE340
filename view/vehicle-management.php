3f765c1ac8e78dfc7bdabe33cd1ec918804c0db9
Ulises Mariano Melgarejo <73831325+Nemo3003@users.noreply.github.com>
2022-05-24 11:27:43 -0300

    enhancement 4: almost there

--- view/vehicle-management.php
+++ view/vehicle-management.php
@@ -24,16 +24,6 @@
                 <li><a class="largeLink" href="/phpmotors/vehicles/index.php?action=Addclassification">Add Classification</a></li>
                 <li><a class="largeLink" href="/phpmotors/vehicles/index.php?action=vehicle">Add Vehicle</a></li>
             </ul>
-            <?php
-                if (isset($message)) { 
-                echo $message; 
-                } 
-                if (isset($classificationList)) { 
-                echo '<h2>Vehicles By Classification</h2>'; 
-                echo '<p class="subHeading">Choose a classification to see those vehicles</p>'; 
-                echo $classificationList; 
-                }
-            ?>
         </div>
     </main>
     <footer>
@@ -42,4 +32,3 @@
 
 </body>
 </html>
-<?php unset($_SESSION['message']); ?>
