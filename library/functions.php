f36f5d2a251234058c898e581e0a84ede7fd9ef2
Ulises Mariano Melgarejo <73831325+Nemo3003@users.noreply.github.com>
2022-05-28 09:12:02 -0300

    adding validations

--- library/functions.php
+++ library/functions.php
@@ -1,2 +1,12 @@
 <?php
-echo "hello"
+function checkEmail($clientEmail){
+  //The filter_var() function filters a variable with the specified filter.
+ $valEmail = filter_var($clientEmail, FILTER_VALIDATE_EMAIL);
+ return $valEmail;
+}
+
+function checkPass($clientPassword){
+  $pattern = '/^(?=.*[[:digit:]])(?=.*[[:punct:]\s])(?=.*[A-Z])(?=.*[a-z])(?:.{8,})$/';
+  //The preg_match() function returns whether a match was found in a string.
+  return preg_match($pattern, $clientPassword);
+ }
