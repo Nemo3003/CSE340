8e3843ac44ec58f97dc8c7d226faddff00df344e
Nemo3003 <ulimarian2000@gmail.com>
2022-06-25 07:31:25 -0300

    enhancement 8

---
+++ view/404.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <link href="https://fonts.googleapis.com/css2?family=Teko&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@400&display=swap" rel="stylesheet">
+  <title>PHP Motors | Error 500</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" media="screen" href="../css/base.css">
+  <link rel="stylesheet" media="screen" href="../css/large.css">
+</head>
+
+
+<body class="body">
+    <div class="content">
+        <header class="header"> 
+            <?php require '../snippets/header.php'?>   
+            <nav id ="page_nav" > 
+            <?php require '../snippets/nav.php'; ?> 
+        </nav>
+        </header>
+        <main class="main">
+            <h1 class="fif-error">404 Vehicle Not found</h1>
+            <h2 class="title-error">Sorry, seems like the vehicle has not being found!<h2> 
+        </main>
+        <footer class="footer">
+            <?php require '../snippets/footer.php'?>   
+        </footer>
+    </div>
+    <script src="../js/app.js"></script>
+</body>
