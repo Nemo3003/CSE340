570d2db9dc01d6cafda9ea1413c57c9a97692ff9
Nemo3003 <ulimarian2000@gmail.com>
2022-07-01 10:33:00 -0300

    Enhancement 9: trying

--- vehicles/index.php
+++ vehicles/index.php
@@ -5,6 +5,7 @@ session_start();
     require_once '../library/connections.php';
     require_once '../model/main-model.php';
     require_once '../model/vehicles-model.php';
+    require_once '../model/uploads-model.php';
     require_once '../library/functions.php';
 
     // Get the array of classifications
@@ -163,6 +164,7 @@ session_start();
             case 'VehicleInformations':
                 $invId = filter_input(INPUT_GET, 'invId', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
                 $invInfo = getInvItemInfo($invId);
+                $thumInfo = getImageThumbByClassification($invId);
                 if(!$invInfo) {
                     $_SESSION['message'] = "<h2 class='notice'>Vehicle could not be found.</h2>";
                     http_response_code(404);
